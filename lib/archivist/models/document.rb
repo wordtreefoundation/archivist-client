db6cc46e1eb35ba2d0fa7c56bdbd028566041149
Duane Johnson <duane.johnson@gmail.com>
2013-11-07 16:37:42 -0700

    Remove Downloader class and put methods in Document model

--- lib/archivist/models/document.rb
+++ lib/archivist/models/document.rb
@@ -12,11 +12,35 @@ module Archivist
       attribute :languages, Array[String]
       attribute :creators, Array[String]
 
+      def initialize
+        @conn = Faraday.new(:url => "http://archive.org") do |faraday|
+          faraday.use FaradayMiddleware::FollowRedirects
+          faraday.request  :url_encoded             # form-encode POST params
+          # faraday.response :logger                  # log requests to STDOUT
+          faraday.adapter  Faraday.default_adapter  # make requests with Net::HTTP
+        end
+      end
+
+      def format_index
+        response = @conn.get(index_xml_path)
+        Model::FormatIndex.new.tap do |idx|
+          Representation::FormatIndex.new(idx).from_xml(response.body)
+        end
+      end
+
+      def download(format=:text)
+        # e.g. format_index.text_format
+        file_format = format_index.send(:"#{format}_format")
+        # e.g. /download/firstbooknapole00gruagoog/firstbooknapole00gruagoog_djvu.txt
+        @conn.get(download_path(file_format.name)).
+          body.force_encoding('UTF-8')
+      end
+
       def download_path(file)
         "/download/#{identifier}/#{file}"
       end
 
-      def download_xml_path
+      def index_xml_path
         download_path("#{identifier}_files.xml")
       end
     end
--- spec/models/document_spec.rb
+++ spec/models/document_spec.rb
@@ -9,6 +9,19 @@ describe Archivist::Model::Document do
 
   it "has download_xml_path" do
     document.identifier = "xyz"
-    document.download_xml_path.should == "/download/xyz/xyz_files.xml"
+    document.index_xml_path.should == "/download/xyz/xyz_files.xml"
+  end
+
+  context "downloading" do
+    before do
+      document.identifier = "firstbooknapole00gruagoog"
+    end
+
+    it "downloads a book" do
+      VCR.use_cassette('download') do
+        book = document.download
+        book[10021..10050].should == "pilots the vessel and weathers"
+      end
+    end
   end
 end
